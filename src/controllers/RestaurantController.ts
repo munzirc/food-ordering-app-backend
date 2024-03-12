af41c03d462c986bf3f88a36e05c11b192423c7e
= <=>
2024-03-12 17:44:06 +0530

    adding checkout flow

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,6 +1,23 @@
 import { Request, Response, response } from "express";
 import Restaurant from "../models/restaurant";
 
+const getRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurantId = req.params.restaurantId;
+
+    const restaurant = await Restaurant.findById(restaurantId);
+    if(!restaurant) {
+      return res.status(404).json({ message: "restaurant not found"});
+    }
+
+    res.json(restaurant);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+}
+
 const serachRestaurant = async (req: Request, res: Response) => {
   try {
     const city = req.params.city;
@@ -70,4 +87,5 @@ const serachRestaurant = async (req: Request, res: Response) => {
 
 export default {
   serachRestaurant,
+  getRestaurant,
 };
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from './routes/MyUserRoute';
 import myRestaurantRoute from './routes/MyRestaurantRoute';
 import restaurantRoute from './routes/RestaurantRoute';
 import { v2 as cloudinary } from  'cloudinary';
+import orderRoute from './routes/OrderRoute';
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
@@ -19,8 +20,11 @@ cloudinary.config({
 });
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+app.use('/api/order/checkout/webhook', express.raw({ type: "*/*"}));
+
+app.use(express.json());
 
 app.get('/health', async (req: Request, res: Response) => {
    res.send({ message: 'health Ok!'});
@@ -28,7 +32,8 @@ app.get('/health', async (req: Request, res: Response) => {
 
 app.use('/api/my/user', myUserRoute);
 app.use('/api/my/restaurant', myRestaurantRoute);
-app.use('/api/restaurant', restaurantRoute)
+app.use('/api/restaurant', restaurantRoute);
+app.use('/api/order', orderRoute);
 
 app.listen(3000, ()=> {
     console.log('Server started on localhost:3000');

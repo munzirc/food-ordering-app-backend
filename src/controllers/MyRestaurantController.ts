9a1d3b371486762f439ce077ee2c1b768710f9c2
= <=>
2024-03-14 17:06:06 +0530

    adding view order features

--- src/controllers/MyRestaurantController.ts
+++ src/controllers/MyRestaurantController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Restaurant from "../models/restaurant";
 import cloudinary from "cloudinary";
 import mongoose from "mongoose";
+import Order from "../models/order";
 
 const getMyRestaurant = async (req: Request, res: Response) => {
   try {
@@ -47,8 +48,8 @@ const updateMyRestaurant = async (req: Request, res: Response) => {
   try {
     const restaurant = await Restaurant.findOne({ user: req.userId });
 
-    if(!restaurant) {
-       return res.status(404).json({ message: "Restaurant not found!"});
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found!" });
     }
 
     restaurant.restaurantName = req.body.restaurantName;
@@ -60,7 +61,7 @@ const updateMyRestaurant = async (req: Request, res: Response) => {
     restaurant.menuItems = req.body.menuItems;
     restaurant.lastUpdated = new Date();
 
-    if(req.file) {
+    if (req.file) {
       const imageUrl = await uploadImage(req.file as Express.Multer.File);
       restaurant.imageUrl = imageUrl;
     }
@@ -68,10 +69,54 @@ const updateMyRestaurant = async (req: Request, res: Response) => {
     await restaurant.save();
 
     res.status(200).send(restaurant);
-    
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+const getMyRestaurantOrders = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant not found" });
+    }
+
+    const orders = await Order.find({ restaurant: restaurant._id })
+      .populate("restaurant")
+      .populate("user");
+
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+};
+
+const updateOrderStatus = async (req: Request, res: Response) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    const restaurant = await Restaurant.findById(order.restaurant);
+
+    if (restaurant?.user?._id.toString() !== req.userId) {
+      return res.status(401).send();
+    }
+
+    order.status = status;
+    await order.save();
+    res.status(200).json(order);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Unable to update order status" });
   }
 };
 
@@ -82,10 +127,12 @@ const uploadImage = async (file: Express.Multer.File) => {
 
   const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
   return uploadResponse.url;
-}
+};
 
 export default {
   createMyRestaurant,
   getMyRestaurant,
   updateMyRestaurant,
+  getMyRestaurantOrders,
+  updateOrderStatus,
 };
--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -7,6 +7,19 @@ const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
 const STRIPE_ENDPOINT_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string;
 
+const getMyOrders = async (req: Request, res: Response) => {
+  try {
+    
+    const orders = await Order.find({ user: req.userId }).populate("restaurant").populate("user");
+
+    res.json(orders);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
 type CheckoutSessionRequest = {
   cartItems: {
     menuItemId: string;
@@ -32,22 +45,21 @@ const stripeWebhookHandler = async (req: Request, res: Response) => {
       sig as string,
       STRIPE_ENDPOINT_SECRET
     );
-
   } catch (error: any) {
     console.log(error);
     return res.status(400).send(`Webhook error: ${error.message}`);
   }
 
-  if(event.type === "checkout.session.completed") {
-      const order = await Order.findById(event.data.object.metadata?.orderId);
-      if(!order) {
-        return res.status(404).json({ message: "Order not found"});
-      }
+  if (event.type === "checkout.session.completed") {
+    const order = await Order.findById(event.data.object.metadata?.orderId);
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
 
-      order.totalAmount = event.data.object.amount_total;
-      order.status = "paid";
+    order.totalAmount = event.data.object.amount_total;
+    order.status = "paid";
 
-      await order.save(); 
+    await order.save();
   }
 
   res.status(200).send();
@@ -162,4 +174,5 @@ const createSession = async (
 export default {
   createCheckoutSession,
   stripeWebhookHandler,
+  getMyOrders,
 };
